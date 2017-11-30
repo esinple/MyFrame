1ff85eaa66a21f16ef42278c4a310c5d4bb44ce7
unknown <shensijiang@gt.com>
2017-11-30 19:16:30 +0800

    TimeTick

---
+++ Server/src/base/ModuleTimeTick.cpp
@@ -0,0 +1,58 @@
+#include "ModuleTimeTick.h"
+
+ModuleTimeTick::ModuleTimeTick()
+{
+}
+
+ModuleTimeTick::~ModuleTimeTick()
+{
+}
+
+const meplay::MPTime& ModuleTimeTick::GetLastTick()const
+{
+	return m_LastTick;
+}
+
+bool ModuleTimeTick::Tick(const meplay::MPTime& now, std::chrono::milliseconds& msec)
+{
+	if (now.CurrentMSec() >= m_LastTick.CurrentMSec() + msec.count())
+	{
+		m_LastTick = now;
+		return true;
+	}
+
+	return false;
+}
+
+bool ModuleTimeTick::Tick(const meplay::MPTime& now, std::chrono::seconds& sec)
+{
+	if (now.CurrentSec() >= m_LastTick.CurrentSec() + sec.count())
+	{
+		m_LastTick = now;
+		return true;
+	}
+
+	return false;
+}
+
+bool ModuleTimeTick::Tick(const meplay::MPTime& now, std::chrono::minutes& min)
+{
+	if (now.CurrentSec() >= m_LastTick.CurrentSec() + min.count() * 60)
+	{
+		m_LastTick = now;
+		return true;
+	}
+
+	return false;
+}
+
+bool ModuleTimeTick::Tick(const meplay::MPTime& now, std::chrono::hours& hour)
+{
+	if (now.CurrentSec() >= m_LastTick.CurrentSec() + hour.count() * 60 * 60)
+	{
+		m_LastTick = now;
+		return true;
+	}
+
+	return false;
+}
---
+++ Server/src/base/ModuleTimeTick.h
@@ -0,0 +1,18 @@
+#pragma once
+#include "MPTime.h"
+
+class ModuleTimeTick
+{
+public:
+	ModuleTimeTick();
+	~ModuleTimeTick();
+public:
+	const meplay::MPTime& GetLastTick()const;
+
+	bool Tick(const meplay::MPTime& now,std::chrono::milliseconds& msec);
+	bool Tick(const meplay::MPTime& now,std::chrono::seconds& sec);
+	bool Tick(const meplay::MPTime& now,std::chrono::minutes& min);
+	bool Tick(const meplay::MPTime& now,std::chrono::hours& hours);
+private:
+	meplay::MPTime m_LastTick;
+};
